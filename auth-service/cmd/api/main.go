e04c9ad417ff95af15be98deed6ce9379215cc25
weilyuwang <weilyuwang@gmail.com>
2022-07-25 22:00:08 -0400

    set up authentication service and models

---
+++ auth-service/cmd/api/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"auth/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct {
+	DB     *sql.DB
+	Models data.Models
+}
+
+func main() {
+	log.Println("Starting authentication service")
+
+	// TODO: connect to DB
+
+	// set up config
+	app := Config{}
+
+	// define http server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	// start the server
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
---
+++ auth-service/go.mod
@@ -0,0 +1,9 @@
+module auth
+
+go 1.18
+
+require (
+	github.com/go-chi/chi/v5 v5.0.7
+	github.com/go-chi/cors v1.2.1
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa
+)
